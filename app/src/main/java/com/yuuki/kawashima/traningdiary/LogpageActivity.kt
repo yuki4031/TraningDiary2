51f94450aa0c0285642994345c0255c0b414b556
Yuki Kawashima <yukikawashima1231@gmail.com>
2020-09-22 21:03:51 +0900

    Logpageの追加

---
+++ app/src/main/java/com/yuuki/kawashima/traningdiary/LogpageActivity.kt
@@ -0,0 +1,11 @@
+package com.yuuki.kawashima.traningdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogpageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logpage)
+    }
+}
